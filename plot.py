7316b3c129c62d5a367637593d31c7a2c2ec280f
Eason <ejin458@aucklanduni.ac.nz>
2025-03-20 10:50:56 +1300

    refactor

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 file_path = 'data.csv'
 data = pd.read_csv(file_path, header=None)
@@ -15,4 +16,23 @@ plt.ylabel('Y')
 plt.title('Plot of Data.csv')
 
 plt.savefig("data_plot.jpg")
+plt.show()
+
+angles = []
+for i in range(1, len(x_coords) - 1):
+    # Vectors between consecutive points
+    vec1 = np.array([x_coords[i] - x_coords[i-1], y_coords[i] - y_coords[i-1]])
+    vec2 = np.array([x_coords[i+1] - x_coords[i], y_coords[i+1] - y_coords[i]])
+    
+    # Calculate the angle between the vectors
+    angle = np.arctan2(vec2[1], vec2[0]) - np.arctan2(vec1[1], vec1[0])
+    angles.append(angle)
+
+# Plot the histogram of angles
+plt.figure()
+plt.hist(angles, bins=30, edgecolor='black')
+plt.xlabel('Angle (degrees)')
+plt.ylabel('Frequency')
+plt.title('Histogram of Angles Between Consecutive Lines')
+plt.savefig("angles_histogram.jpg")
 plt.show()
--- src/autonomous_take_home_project/src/main.cpp
+++ src/autonomous_take_home_project/src/main.cpp
@@ -19,11 +19,15 @@ public:
         pub = this->create_publisher<msgs::msg::Response>("response", 10);
 
         last_time = rclcpp::Time(0, 0);
-        geometry_msgs::msg::Point last_point_2;
+
+        last_point_1.x = 0.0;
+        last_point_1.y = 0.0;
+        last_point_1.z = 0.0;
+
         last_point_2.x = 0.0;
         last_point_2.y = 0.0;
         last_point_2.z = 0.0;
-        geometry_msgs::msg::Vector3 last_velocity;
+
         last_velocity.x = 0.0;
         last_velocity.y = 0.0;
         last_velocity.z = 0.0;
@@ -35,10 +39,8 @@ private:
         // Get time
         int32_t sec = msg->header.stamp.sec;
         uint32_t nanosec = msg->header.stamp.nanosec;
-        rclcpp::Time new_time = rclcpp::Time(sec, nanosec);
 
-        // Get frame
-        std::string frame_id = msg->header.frame_id;
+        rclcpp::Time new_time = rclcpp::Time(sec, nanosec);
 
         // Get location
         double x = msg->location.x;
@@ -46,33 +48,57 @@ private:
 
         msgs::msg::Response response_msg;
         double time_diff = find_time_difference(new_time, last_time);
-        geometry_msgs::msg::Point new_point;
-        new_point.x = x;
-        new_point.y = y;
-        new_point.z = 0.0;
-
-        geometry_msgs::msg::Vector3 new_velocity;
-        new_velocity.x = (x - last_point_2.x) / time_diff;
-        new_velocity.y = (y - last_point_2.y) / time_diff;
-        new_velocity.z = 0.0;
 
         if (time_diff > 0)
         {
-            response_msg.target = create_target(time_diff, new_point, new_velocity);
+            geometry_msgs::msg::Point new_point;
+            new_point.x = x;
+            new_point.y = y;
+            new_point.z = 0.0;
 
-            bool valid = is_valid(last_point_1, last_point_2, new_point);
-            if (valid)
+            geometry_msgs::msg::Vector3 new_velocity;
+            new_velocity.x = (x - last_point_2.x) / time_diff;
+            new_velocity.y = (y - last_point_2.y) / time_diff;
+            new_velocity.z = 0.0;
+
+            geometry_msgs::msg::Vector3 new_acceleration;
+            new_acceleration.x = (new_velocity.x - last_velocity.x) / time_diff;
+            new_acceleration.y = (new_velocity.y - last_velocity.y) / time_diff;
+            new_acceleration.z = 0.0;
+
+            // Create target
+            msgs::msg::Kinematics target;
+
+            target.position = new_point;
+            target.velocity = new_velocity;
+            target.acceleration = new_acceleration;
+
+            double angle = find_angle(new_velocity, last_velocity);
+            target.angle = static_cast<float64_t> angle;
+            double angular_velocity = angle / time_diff;
+            target.anglular_velocity = static_cast<float64_t> angular_velocity;
+
+            response_msg.target = target;
+
+            printf("x: %f\t y: %f\t velocity: %.2f\t acc: %.2f\t angle: %.2f\t ang_v: %.2f\n", x, y, find_magnitude(new_velocity), find_magnitude(new_acceleration), angle, fabs(angular_velocity));
+            const max_speed = 10;
+            const max_acc = 2.5;
+            const max_ang_v = 2.5;
+
+            if (is_valid(last_point_1, last_point_2, new_point) && find_magnitude(new_velocity) <= max_speed && find_magnitude(new_acceleration) <= max_acc && fabs(angular_velocity) <= max_ang_v)
             {
                 lvtl = new_point;
                 response_msg.lvtl = lvtl;
                 response_msg.target_location = new_point;
+
+                // Set valid vehicle points
             }
             else
             {
                 response_msg.target_location = create_midpoint(lvtl, new_point);
-            }
 
-            // Create vehicle Kinematics
+                // Set invalid vehicle points
+            }
 
             // Reset last variables
             last_time = new_time;
@@ -81,26 +107,6 @@ private:
 
             pub->publish(response_msg);
         }
-    }
-
-    msgs::msg::Kinematics create_target(double time_diff, geometry_msgs::msg::Point &new_point, geometry_msgs::msg::Vector3 &new_velocity)
-    {
-        msgs::msg::Kinematics target;
-
-        target.position = new_point;
-
-        target.velocity.x = new_velocity.x;
-        target.velocity.y = new_velocity.y;
-        target.velocity.z = 0.0;
-
-        target.acceleration.x = (new_velocity.x - last_velocity.x) / time_diff;
-        target.acceleration.y = (new_velocity.y - last_velocity.y) / time_diff;
-        target.acceleration.z = 0.0;
-
-        double angle_diff = find_angle_difference(new_velocity, last_velocity);
-        target.angle = angle_diff;
-        target.anglular_velocity = angle_diff / time_diff;
-        return target;
     }
 
     double find_time_difference(rclcpp::Time &new_time, rclcpp::Time &last_time)
@@ -112,12 +118,11 @@ private:
         return sec_diff + nanosec_diff / 1e9;
     }
 
-    double find_angle_difference(const geometry_msgs::msg::Vector3 &new_vec,
-                                 const geometry_msgs::msg::Vector3 &last_vec)
+    double find_angle(const geometry_msgs::msg::Vector3 &new_vec,
+                      const geometry_msgs::msg::Vector3 &last_vec)
     {
         double dot_product = new_vec.x * last_vec.x + new_vec.y * last_vec.y;
-        double magnitude_new_vec = std::sqrt(new_vec.x * new_vec.x + new_vec.y * new_vec.y);
-        double magnitude_last_vec = std::sqrt(last_vec.x * last_vec.x + last_vec.y * last_vec.y);
+        double magnitude_new_vec = find_magnitude(new_vec) double magnitude_last_vec = find_magnitude(last_vec);
 
         if (magnitude_new_vec == 0.0 || magnitude_last_vec == 0.0)
         {
@@ -127,7 +132,7 @@ private:
         double cos_theta = dot_product / (magnitude_new_vec * magnitude_last_vec);
         cos_theta = std::max(-1.0, std::min(1.0, cos_theta));
         double cross_product = new_vec.x * last_vec.y - new_vec.y * last_vec.x;
-        double angle = std::atan2(cross_product, dot_product) * (180.0 / M_PI);
+        double angle = std::atan2(cross_product, dot_product);
 
         return angle;
     }
@@ -153,29 +158,35 @@ private:
         return midpoint;
     }
 
+    double find_magnitude(geometry_msgs::msg::Vector3 vector)
+    {
+        return std::sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+    }
+
     bool is_valid(geometry_msgs::msg::Point &p1, geometry_msgs::msg::Point &p2, geometry_msgs::msg::Point &p3)
     {
-        // Consider the case for the first two points
-
-        // Given 3 points {p1, p2, p3}, if angle between (p2-p1) and (p2-p3) is too small, this target is invalid
-        const int threshold = 90;
-        if ((find_angle_difference(create_vector_from_points(p2, p3), create_vector_from_points(p1, p2))) > threshold)
+        // Consider the case for the first time
+        geometry_msgs::msg::Vector3 vec1 = create_vector_from_points(p2, p3);
+        geometry_msgs::msg::Vector3 vec2 = create_vector_from_points(p1, p2);
+        double magnitude_vec1 = find_magnitude((vec1));
+        double magnitude_vec2 = find_magnitude(vec2);
+        if (magnitude_vec1 == 0.0 || magnitude_vec2 == 0.0)
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+
+        // Given 3 points {p1, p2, p3}, if angle between (p2-p1) and (p2-p3) is too small, this target is invalid
+        const int threshold = 2.5; // From the brief
+        return (fabs(find_angle(vec1, vec2)) <= threshold) ? true : false;
     }
     rclcpp::Subscription<msgs::msg::LocationStamped>::SharedPtr sub;
     rclcpp::Publisher<msgs::msg::Response>::SharedPtr pub;
 
-    geometry_msgs::msg::Point lvtl;
+    rclcpp::Time last_time;
     geometry_msgs::msg::Point last_point_1; // This is at t-2
     geometry_msgs::msg::Point last_point_2; // This is at t-1
     geometry_msgs::msg::Vector3 last_velocity;
-    rclcpp::Time last_time;
+    geometry_msgs::msg::Point lvtl;
 };
 
 int main(int argc, char **argv)
